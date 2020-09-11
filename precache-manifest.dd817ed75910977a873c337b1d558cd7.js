419ae5cb57286ac29042d16a988c1e2cacb40e66
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2020-09-11 15:19:02 +0800

    Updates

---
+++ precache-manifest.dd817ed75910977a873c337b1d558cd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39879dc9ba7fa60f6494e4a79cd31cc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "702289693446b33791da",
+    "url": "/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "1b58df710cad20cd8fe7",
+    "url": "/static/css/main.ced3917f.chunk.css"
+  },
+  {
+    "revision": "702289693446b33791da",
+    "url": "/static/js/2.ff37e0ea.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/static/js/2.ff37e0ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b58df710cad20cd8fe7",
+    "url": "/static/js/main.6e4c02da.chunk.js"
+  },
+  {
+    "revision": "46081101a5577cab6109",
+    "url": "/static/js/runtime-main.c524012f.js"
+  }
+]);
