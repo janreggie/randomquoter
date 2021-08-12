c37335c70f1cfd9bb1f63aced0b85e50faf06106
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-08-12 11:58:57 +0800

    App now function component

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,12 @@ import Header from './Header';
 import Footer from './Footer';
 import QuoteContainer from './QuoteContainer';
 
-function App() {
-  return (
-    <div id="App">
-      <Header />
-      <QuoteContainer />
-      <Footer />
-    </div>
-  );
-}
+const App = () => (
+  <div id="App">
+    <Header />
+    <QuoteContainer />
+    <Footer />
+  </div>
+);
 
 export default App;
