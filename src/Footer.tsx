be783b38204694a03386274977acb428b27a6eb6
Jan Reggie Dela Cruz <janreggie.delacruz@gmail.com>
2021-08-12 11:57:23 +0800

    Header, Footer now function components

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -3,21 +3,19 @@ import React from 'react';
 /**
  * Footer represents the footer of randomquoter
  */
-function Footer(): JSX.Element {
-  return (
-    <footer id="footer" className="footer small bg-secondary text-white text-center" >
-      <p>
-        Copyright © 2020 Jan Reggie Dela Cruz and contributors.
-        Project for the Front End Libraries Certification in <a href="https://www.freecodecamp.org/learn/">freeCodeCamp</a>.
-      </p>
-      <p>
-        You may view the source code of this project over at <a href="https://github.com/janreggie/randomquoter">github.com/janreggie/randomquoter</a>.
-      </p>
-      <p>
-        All quotes from this project have been sourced from <a href="https://github.com/lukePeavey/quotable">lukePeavey/quotable</a>.
-      </p>
-    </footer>
-  )
-}
+const Footer = () => (
+  <footer id="footer" className="footer small bg-secondary text-white text-center" >
+    <p>
+      Copyright © 2020 Jan Reggie Dela Cruz and contributors.
+      Project for the Front End Libraries Certification in <a href="https://www.freecodecamp.org/learn/">freeCodeCamp</a>.
+    </p>
+    <p>
+      You may view the source code of this project over at <a href="https://github.com/janreggie/randomquoter">github.com/janreggie/randomquoter</a>.
+    </p>
+    <p>
+      All quotes from this project have been sourced from <a href="https://github.com/lukePeavey/quotable">lukePeavey/quotable</a>.
+    </p>
+  </footer>
+)
 
 export default Footer;
--- src/Header.tsx
+++ src/Header.tsx
@@ -3,13 +3,11 @@ import React from 'react';
 /**
  * Header represents the header of randomquoter
  */
-function Header(): JSX.Element {
-  return (
-    <div id="header" className="jumbotron text-white bg-primary">
-      <h1 className="display-4">Random Quote Machine</h1>
-      <p className="lead">Generate random quotes out of thin air!</p>
-    </div>
-  )
-}
+const Header = () => (
+  <div id="header" className="jumbotron text-white bg-primary">
+    <h1 className="display-4">Random Quote Machine</h1>
+    <p className="lead">Generate random quotes out of thin air!</p>
+  </div>
+)
 
 export default Header;
